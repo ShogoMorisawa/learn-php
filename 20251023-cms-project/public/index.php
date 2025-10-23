7500aaf55ecf3e84103cc0c52c10b50c3885bd1c
Shogo Morisawa <mo21s1hc@apu.ac.jp>
2025-10-23 20:48:23 +0900

    2025-cms-project/ first commit

---
+++ 20251023-cms-project/public/index.php
@@ -0,0 +1,13 @@
+<?php
+// まず最初にComposerのオートローダーを読み込む（超重要）
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Shogomorisawa\Project\Router;
+
+// ブラウザがリクエストしたURLとHTTPメソッドを取得
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+// ルーターを使用してリクエストを処理
+$router = new Router();
+echo $router->handleRequest($requestMethod, $requestUri);
