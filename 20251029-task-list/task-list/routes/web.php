03c73c53b747c5fbeb2bec2d529b786abb4750e4
Shogo Morisawa <mo21s1hc@apu.ac.jp>
2025-10-30 13:21:15 +0900

    20251029-task-list/feat: ルーティングの学習

--- 20251029-task-list/task-list/routes/web.php
+++ 20251029-task-list/task-list/routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hello World';
+});
+
+Route::get('/users', function (){
+    return 'Users page';
+});
+
+Route::get('/shogo', function(){
+    return 'Shogo page';
+});
+
+Route::get('/users/{name}', function($name){
+    return 'Hello' .' '. $name;
+});
+
+Route::fallback(function(){
+    return '404 Not Found';
 });
