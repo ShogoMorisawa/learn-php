730f08887c2d072796c2b3d0786aceba964d8f20
Shogo Morisawa <mo21s1hc@apu.ac.jp>
2025-11-06 00:44:00 +0900

    20251101-book-reviews/feat: レビュー投稿をレート制限して濫用を防止
      - AppServiceProviderで reviews 用のレートリミッタを追加し、1時間3件までに制限
      - レビュー作成/登録ルートへ throttle:reviews ミドルウェアを適用し設定を有効化

--- 20251101-book-reviews/book-reviews/routes/web.php
+++ 20251101-book-reviews/book-reviews/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\ReviewController;
+use Illuminate\Support\Facades\RateLimiter;
 
 Route::get('/', function () {
     redirect()->route('books.index');
@@ -10,4 +11,4 @@ Route::get('/', function () {
 
 Route::resource('books', BookController::class)->only(['index', 'show']);
 
-Route::resource('books.reviews', ReviewController::class)->scoped(['review' => 'book'])->only(['create', 'store']);
+Route::resource('books.reviews', ReviewController::class)->scoped(['review' => 'book'])->only(['create', 'store'])->middleware('throttle:reviews');
