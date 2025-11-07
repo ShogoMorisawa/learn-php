7578f3472834f142ea6debbf00f2dd77133b7df4
Shogo Morisawa <mo21s1hc@apu.ac.jp>
2025-11-07 23:57:48 +0900

    20251106-events-app/feat: イベントと参加者のモデル・シーディングを追加

---
+++ 20251106-events-app/events-app/app/Models/Event.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    /** @use HasFactory<\Database\Factories\EventFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'start_time',
+        'end_time',
+        'user_id',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'start_time' => 'datetime',
+            'end_time' => 'datetime',
+        ];
+    }
+}
+
--- 20251106-events-app/events-app/database/seeders/DatabaseSeeder.php
+++ 20251106-events-app/events-app/database/seeders/DatabaseSeeder.php
@@ -17,9 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory(1000)->create();
+
+        $this->call([
+            EventSeeder::class,
+            AttendeeSeeder::class,
         ]);
     }
 }
