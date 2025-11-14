a5e39a0dff04e6448bf27815f0e97a0fc14b63ea
Shogo Morisawa <mo21s1hc@apu.ac.jp>
2025-11-14 22:34:57 +0900

    20251112-job-board/fix: ログアウト導線を整理
      - app/Http/Controllers/AuthController.php:1 destroy() で Auth::logout() + セッション無効化 + トークン再生成
        を行い、トップへリダイレクトする実装に変更
      - resources/views/components/layout.blade.php:1 ナビゲーションを追加し、ログイン時はユーザー名とログアウト
        フォーム（route('logout')）を表示、未ログイン時はサインイン導線を出すよう更新
      - routes/web.php:1 認証ルートを整理し、logout 用 DELETE ルートを定義し直して auth リソースから destroy を
        除外

--- 20251112-job-board/routes/web.php
+++ 20251112-job-board/routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
+// 求人関連のルート
 Route::redirect('/', '/jobs');
-
 Route::resource('jobs', JobController::class)->only(['index', 'show']);
 
+// 認証関連のルート
 Route::get('login', fn () => to_route('auth.create'))->name('login');
+Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
 Route::resource('auth', AuthController::class)->only(['create', 'store']);
