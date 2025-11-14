26d679fab74e5aa5bc33adfbef0c9a8c67c5344f
Shogo Morisawa <mo21s1hc@apu.ac.jp>
2025-11-14 23:10:26 +0900

    20251112-job-board/feat: 応募送信と通知を実装
      - app/Http/Controllers/JobApplicationController.php:1 store アクションを追加し、応募フォームのバリデーション
        →ジョブ応募レコード作成→詳細画面へのリダイレクト＋成功メッセージ付与を実装
      - app/Models/JobApplication.php:1  に expected_salary/user_id/job_id を追加し、コントローラからの
        create() で一括代入できるよう対応
      - resources/views/components/layout.blade.php:1 セッションの success メッセージをグローバルレイアウトで表示
        するアラートを追加し、応募完了通知がユーザーに伝わるようにした

--- 20251112-job-board/app/Models/JobApplication.php
+++ 20251112-job-board/app/Models/JobApplication.php
@@ -11,13 +11,15 @@ class JobApplication extends Model
     /** @use HasFactory<\Database\Factories\JobApplicationFactory> */
     use HasFactory;
 
-    public function job(): BelongsTo
-    {
-        return $this->belongsTo(Job::class);
-    }
+    protected $fillable = ['expected_salary', 'user_id', 'job_id'];
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function job(): BelongsTo
+    {
+        return $this->belongsTo(Job::class);
     }
 }
