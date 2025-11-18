29c14a50b962c679b2789130409a78968e30c7fe
Shogo Morisawa <mo21s1hc@apu.ac.jp>
2025-11-18 18:12:57 +0900

    20251112-job-board/chore: ラジオ入力にもバリデーションエラー表示を追加
      - resources/views/components/radio-group.blade.php:1 @error() ブロックを追加し、ラジオグループの直下に
        バリデーションエラーを表示できるようにした。

--- 20251112-job-board/resources/views/components/radio-group.blade.php
+++ 20251112-job-board/resources/views/components/radio-group.blade.php
@@ -11,4 +11,9 @@
             <span class="ml-2">{{ $label }}</span>
         </label>
     @endforeach
+    @error($name)
+        <div class="mt-1 text-xs text-red-500">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
